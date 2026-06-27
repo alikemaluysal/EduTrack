3986f05c54fefe095c3fb0c1de410aa1db0d101f
Ali Kemal Uysal <alikemaluysal4235@gmail.com>
2026-06-27 09:53:45 +0300

    feat(persistence): add dbContext

---
+++ src/EduTrack.Persistence/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EduTrack.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace EduTrack.Persistence;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<CourseStudent> CourseStudent { get; set; }
+    
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
