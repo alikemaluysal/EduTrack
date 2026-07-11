017e7cf312fc97eeac4ff0ef9581cd01317d5391
Ali Kemal Uysal <alikemaluysal4235@gmail.com>
2026-07-11 10:35:39 +0300

    feat: add file api

---
+++ external-services/FileAPI/Program.cs
@@ -0,0 +1,42 @@
+using FileAPI.Middleware;
+using Microsoft.OpenApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.ApiKey,
+        Name = "X-Api-Key",
+        In = ParameterLocation.Header,
+        Description = "Upload ve silme işlemleri için API anahtarı"
+    });
+    options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+    {
+        [new OpenApiSecuritySchemeReference("ApiKey", document, null)] = []
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (builder.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseMiddleware<FileApiKeyMiddleware>();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
