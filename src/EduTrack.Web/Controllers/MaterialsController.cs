18f239a3b11c4dd2bb069cfe4457ccb5641b8303
Ali Kemal Uysal <alikemaluysal4235@gmail.com>
2026-07-11 11:44:51 +0300

    feat: add material create page

---
+++ src/EduTrack.Web/Controllers/MaterialsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduTrack.Web.Controllers;
+
+public class MaterialsController : BaseController
+{
+    [HttpGet]
+    [Authorize]
+    public IActionResult Add(Guid id)
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Authorize]
+    public IActionResult Add(Guid id, string material)
+    {
+
+        return View();
+    }
+}
