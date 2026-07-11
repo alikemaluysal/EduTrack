17d56531cef4a3c4fbbada90bad97d193b870ef4
Ali Kemal Uysal <alikemaluysal4235@gmail.com>
2026-07-11 11:33:58 +0300

    feat: file api integration

---
+++ src/EduTrack.Application/DTOs/File/FileUploadRequest.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EduTrack.Application.DTOs.File;
+
+public record FileUploadRequest(Stream Content, string FileName, string? ContentType);
---
+++ src/EduTrack.Application/DTOs/File/FileUploadResult.cs
@@ -0,0 +1,3 @@
+﻿namespace EduTrack.Application.DTOs.File;
+
+public record FileUploadResult(string FileId, string OriginalFileName, long Length, string OpenUrl, string DownloadUrl);
